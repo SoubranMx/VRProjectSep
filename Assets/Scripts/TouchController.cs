5dd2b267d91416547be5217e81d0b09fb12b8107
SoubranMx <uriel_bee15@hotmail.com>
2019-09-14 22:31:57 -0500

    Raycast
    Implementación de raycast para desaparecer bloques.

---
+++ Assets/Scripts/TouchController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetTouch(0).phase == 0)
+        {
+            // raycast
+            RaycastHit touched;
+
+            Physics.Raycast(Camera.main.ScreenPointToRay(Input.GetTouch(0).position),out touched);//Guarda en el apuntador touched mediante out, es como si hicieras touched = blablabla(getTouch)
+            //Deshabilitar objeto CON COLLIDER.
+            //touched.collider.gameObject.SetActive(false);
+
+            //Box Collider
+            touched.collider.gameObject.SetActive(false);
+
+            //Cambiar material
+            //var renderer = touched.collider.gameObject.GetComponent<MeshRenderer>();
+            //renderer.material.color = new Color32((byte)211,(byte)28, (byte)28, (byte)255);
+
+
+        }
+    }
+}
